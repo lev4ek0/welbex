91e0a58c06a3b1efffc0719c67ed35e98681481f
Lev Belous <leva22.08.01@inbox.ru>
2022-03-03 22:00:58 +0300

    static_root

--- welbex/settings.py
+++ welbex/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'front/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'front/static'
